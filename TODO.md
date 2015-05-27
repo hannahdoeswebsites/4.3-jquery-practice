2c97fafb6628cfa68456679d7c8f75fe21ec1c5d
Hannah Jordan <hjorda0126@aol.com>
2015-05-27 15:13:19 -0400

    Added accordion javascript. Still needs some styling. All open when they should and close when another is clicked on.

---
+++ TODO.md
@@ -0,0 +1,13 @@
+[ ] Set up containers
+[ ] Get into put into containers, such as OL names and LI names
+[ ] Add coloring as needed.
+[ ] Add icons where necessary
+[ ] Create the acordion movement.
+
+ol {
+    list-style-type: decimal-leading-zero;
+    @include span-columns(4);
+    margin: 0 auto;
+    padding-left: 0;
+    background-color: $dark-gray;
+    color: white;
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,26 @@
+
 (function(){
   'use strict';
 
-  $(document).ready(function(){
-    $('body').prepend(JST['application']());
+
+
+  $(document).ready(function() {
+    
+
+      $("ol").slideUp(0);
+
+      $("h3").click(function(e) {
+          // slideUp the sibling ol
+          $("h3").removeClass('active');
+          $("ol").slideUp(300).removeClass('open');
+          $(this).addClass('active');
+          // Open up the hidden content panel
+          $(this).siblings("div").find("ol").slideDown(300).addClass('open');
+
+
+          e.preventDefault();
+      });
   });
+
+
 })();
